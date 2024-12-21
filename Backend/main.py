e4cb7d78a9660ef26650b6ae35e093ebadec3b0a
KaranMN06 <karanmn06@gmail.com>
2024-12-21 18:53:29 +0530

    "chnages for origins"

--- Backend/main.py
+++ Backend/main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
 from backend.routes.api import router as api_route
-
+from fastapi.middleware.cors import CORSMiddleware
 
 
 
 app = FastAPI(title="Hospital management system",)
+
+origins = [
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins, 
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"],  
+)
 
 
 app.include_router(api_route)
